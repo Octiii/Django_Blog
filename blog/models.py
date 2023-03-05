70b77a49d486d5fb1f7a2f0893ba7618e2e7bc36
Octiii <sirocti@gmail.com>
2023-03-05 11:45:20 +0000

    Modells upgrade

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField 
 # Create your models here.
 
+STATUS = ((0, "Draft"), (1, "Published")) 
 
+
+class Post(models.Model): 
+    title = models.CharField(max_lenght=200, unique=True)
+    slug = models.SlugField(max_lenght=200, unique=True)
