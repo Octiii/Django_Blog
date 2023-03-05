bf9ee86e37aa58f3079364d0701bbdca5afececc
Octiii <sirocti@gmail.com>
2023-03-05 12:52:29 +0000

    Models Done

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,37 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model): 
     title = models.CharField(max_lenght=200, unique=True)
     slug = models.SlugField(max_lenght=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    update_on = models.DateTimeField(auto_now=True)
+    content = models.TextField(blank=True)
+    featured_image = CloudinaryField("image", deafault='palceholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model): 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_lenght=80)
+    email = models.EmailField()
+    body = models.TextField() 
+    created_on = models.DateTimeField(auto_now_add=True)
+    aproved = models.BooleanField(defaoult=False)
+
+    class Meta: 
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
