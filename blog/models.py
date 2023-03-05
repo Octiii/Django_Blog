290de57f9c417d10cf0cf0a5dfc4eb3e4008f975
Octiii <sirocti@gmail.com>
2023-03-05 13:03:37 +0000

    Corections

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,12 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model): 
-    title = models.CharField(max_lenght=200, unique=True)
-    slug = models.SlugField(max_lenght=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     update_on = models.DateTimeField(auto_now=True)
     content = models.TextField(blank=True)
-    featured_image = CloudinaryField("image", deafault='palceholder')
+    featured_image = CloudinaryField("image", default='palceholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -30,11 +30,11 @@ class Post(models.Model):
 
 class Comment(models.Model): 
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_lenght=80)
+    name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField() 
     created_on = models.DateTimeField(auto_now_add=True)
-    aproved = models.BooleanField(defaoult=False)
+    aproved = models.BooleanField(default=False)
 
     class Meta: 
         ordering = ['created_on']
