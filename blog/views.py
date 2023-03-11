264eaa73f01973ab4367ed1b648b5a9047b8dce3
Octiii <sirocti@gmail.com>
2023-03-11 17:43:39 +0000

    Django part 9a - Creating the Post detailed view.

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from .models import Post
 # Create your views here.
 
 
-class PostList(heneric.ListView):
+class PostList(generic.ListView):
     model = Post
-    queryset = Post.object.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
